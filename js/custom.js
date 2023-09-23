c4e0ae271ac0578d7986148d0de0e02bbd1d97f1
Joyreya-Joya <83129132+Joyreya-Joya@users.noreply.github.com>
2023-09-23 23:04:38 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+
+$('.do_icon_ud_one').click(function(){
+    $('.dtbo').slideToggle();
+    $('.ffauo').toggle();
+    $('.ffado').toggle();
+}
+);
+
+$('.do_icon_ud_two').click(function(){
+    $('.dtbt').slideToggle();
+    $('.ffaut').toggle();
+    $('.ffadt').toggle();
+}
+);
+
+$('.do_icon_ud_three').click(function(){
+    $('.dtbth').slideToggle();
+    $('.ffauth').toggle();
+    $('.ffadth').toggle();
+}
+);
+
